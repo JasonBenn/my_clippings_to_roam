f5a45ebeaa25f36956c5771539ca7a4a8e742b8b
Jason Benn <jasoncbenn@gmail.com>
2020-02-24 09:48:56 -0800

    Add a few new cases of regexes, remove BOMs.

--- parse.py
+++ parse.py
@@ -10,7 +10,7 @@ from dateutil import parser
 
 
 # Configure this script by changing these two constants:
-MY_CLIPPINGS_PATH = "/Users/jasonbenn/Calibre Library/Kindle/My Clippings (126)/My Clippings - Kindle.txt"
+MY_CLIPPINGS_PATH = "/Users/jasonbenn/Desktop/Kindle/My Clippings.txt"
 CUSTOM_HASHTAGS = ["ML papers", "TODO"]
 
 
@@ -26,7 +26,7 @@ class Highlight:
 
 
 def suffix(d):
-    return 'th' if 11<=d<=13 else {1:'st',2:'nd',3:'rd'}.get(d%10, 'th')
+    return 'th' if 11 <= d <= 13 else {1: 'st', 2: 'nd', 3: 'rd'}.get(d % 10, 'th')
 
 
 def formatted_date(dt = datetime.now()):
@@ -42,7 +42,7 @@ for annotation in annotations:
     *title_parts, author = title_line.split(" - ")
     title = " - ".join(title_parts)
 
-    page_start, page_end, location_start, location_end, added_str = re.match(r"- Your Highlight on page (\d+)-?(\d+)?(?: \| Location (\d+)-(\d+))? \| Added on (.*)", highlight_line).groups()
+    page_start, page_end, location_start, location_end, added_str = re.match(r"- Your (?:Highlight|Bookmark|Note) on(?: Page (\d+)-?(\d+)?)?(?: \|)?(?: Location (\d+)-?(\d+))? \| Added on (.*)", highlight_line, re.IGNORECASE).groups()
 
     highlights.append(Highlight(
         title_line=title_line.strip(),
@@ -55,7 +55,8 @@ for annotation in annotations:
     ))
 
 
-grouped = {k: list(v) for k, v in groupby(sorted(highlights), attrgetter("title_line"))}
+BYTE_ORDER_MARK = '\ufeff'  # hack warning: decoding with utf-8-sig doesn't remove all BOMs?!
+grouped = {k.strip(BYTE_ORDER_MARK): list(v) for k, v in groupby(sorted(highlights), attrgetter("title_line"))}
 
 if len(sys.argv) > 1:
     query_parts = sys.argv[1:]
